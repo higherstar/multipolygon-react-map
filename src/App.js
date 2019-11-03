03392ac85586cef798f09722650bf6bb3f0268ea
Gold Byol <goldbyol@outlook.com>
2019-11-03 09:28:34 +0200

    intial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="upload-container">
+
+      </div>
+      <div className="map-container">
+
+      </div>
+    </div>
+  );
+}
+
+export default App;

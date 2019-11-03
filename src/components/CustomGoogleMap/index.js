01f45e6075cc5a52dad9c77d734d748f475d5f1c
Gold Byol <goldbyol@outlook.com>
2019-11-03 11:31:37 +0200

    draw polygon from json

--- src/components/CustomGoogleMap/index.js
+++ src/components/CustomGoogleMap/index.js
@@ -2,53 +2,21 @@ import React from "react";
 import GoogleMapReact from "google-map-react";
 import { multiPolygon, polygon } from '@turf/helpers';
 
-const getPosition = (point) => {
-  return { lat: point[0], lng: point[1] };
-};
-
 export const CustomGoogleMap = ({ center, zoom, polygonData }) => {
-  console.log(polygonData);
-  let fc = multiPolygon([
-    [[-24, 63], [-23, 60], [-25, 65], [-20, 69]],
-    [[-14, 43], [-13, 40], [-15, 45], [-10, 49]]
-  ]);
-  let fc1 = polygon([
-    [
-      [
-        92.46093749999999,
-        70.37785394109224
-      ],
-      [
-        41.1328125,
-        53.74871079689897
-      ],
-      [
-        84.72656249999999,
-        23.241346102386135
-      ],
-      [
-        136.40625,
-        39.095962936305476
-      ],
-      [
-        124.1015625,
-        64.32087157990324
-      ],
-      [
-        92.46093749999999,
-        70.37785394109224
-      ]
-    ]
-  ]);
-  console.log(fc1);
+  const mapHandler = React.useRef(null);
+
+  React.useEffect(() => {
+    mapHandler.current && mapHandler.current.data.addGeoJson(polygon(polygonData));
+  }, [polygonData]);
+
   return (
     <GoogleMapReact
       bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAP_API_KEY }}
       defaultCenter={center}
       defaultZoom={zoom}
       onGoogleApiLoaded={({map, maps}) => {
-        console.log(map, maps);
-        map.data.add(fc.geometry);
+        mapHandler.current = map;
+        polygonData && map.data.addGeoJson(polygon(polygonData));
       }}
       yesIWantToUseGoogleMapApiInternals
     >
